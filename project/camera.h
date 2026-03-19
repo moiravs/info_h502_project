f3e9bbeb37514e2c660fc34c787c966d5f8af8ad
moiravs <moira.vanderslagmolen@ulb.be>
2026-03-19 12:50:49 +0100

    [ADD] heightmap

--- project/camera.h
+++ project/camera.h
@@ -5,10 +5,11 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
-#include<glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
+enum Camera_Movement
+{
     FORWARD,
     BACKWARD,
     LEFT,
@@ -18,10 +19,10 @@ enum Camera_Movement {
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 2.5f;
+const float MOV_SPEED = 100.0f;
+const float ROT_SPEED = 2.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
-
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
 class Camera
@@ -38,11 +39,12 @@ public:
     float Pitch;
     // camera options
     float MovementSpeed;
+    float RotationSpeed;
     float MouseSensitivity;
     float Zoom;
 
     // constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(MOV_SPEED), RotationSpeed(ROT_SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         this->Position = position;
         this->WorldUp = up;
@@ -51,7 +53,7 @@ public:
         this->updateCameraVectors();
     }
     // constructor with scalar values
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(MOV_SPEED), RotationSpeed(ROT_SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         this->Position = glm::vec3(posX, posY, posZ);
         this->WorldUp = glm::vec3(upX, upY, upZ);
@@ -66,20 +68,23 @@ public:
         return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
     }
 
-    glm::mat4 GetProjectionMatrix(float fov=45.0, float ratio=1.0, float near=0.01, float far=100.0)
+    glm::mat4 GetProjectionMatrix(float fov = 45.0, float ratio = 1.0, float near = 0.01, float far = 100.0)
     {
         return glm::perspective(fov, ratio, near, far);
     }
 
-    glm::vec3 GetCameraUp() {
+    glm::vec3 GetCameraUp()
+    {
         return this->Up;
     }
 
-    glm::vec3 GetCameraRight() {
+    glm::vec3 GetCameraRight()
+    {
         return this->Right;
     }
 
-    glm::vec3 GetCameraPosition() {
+    glm::vec3 GetCameraPosition()
+    {
         return this->Position;
     }
 
@@ -99,13 +104,12 @@ public:
 
     void ProcessKeyboardRotation(float YawRot, float PitchRot, float deltaTime, GLboolean constrainPitch = true)
     {
-        float velocity = this->MovementSpeed * deltaTime;
+        float velocity = this->RotationSpeed * deltaTime;
         YawRot *= velocity;
         PitchRot *= velocity;
 
         this->Yaw += YawRot;
         this->Pitch += PitchRot;
-
 
         // Make sure that when pitch is out of bounds, screen doesn't get flipped
         if (constrainPitch)
@@ -116,15 +120,14 @@ public:
                 this->Pitch = -89.0f;
         }
         updateCameraVectors();
-
     }
 
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
     void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
     {
         /* The motivated students can implement rotation using the mouse rather than the keyboard
-        * You can draw inspiration from the ProcessKeyboardMovement function
-        */
+         * You can draw inspiration from the ProcessKeyboardMovement function
+         */
     }
 
     // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
@@ -148,7 +151,7 @@ private:
         front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
         Front = glm::normalize(front);
         // also re-calculate the Right and Up vector
-        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        Right = glm::normalize(glm::cross(Front, WorldUp)); // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
         Up = glm::normalize(glm::cross(Right, Front));
     }
 };
