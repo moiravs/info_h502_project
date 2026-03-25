04b92cb6492bb555a79c860bebe5f01f35a7cb50
moiravs <moira.vanderslagmolen@ulb.be>
2026-03-25 12:12:41 +0100

    [REFACTOR] code

---
+++ project/game_engine/displaymanager.h
@@ -0,0 +1,98 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include <glad/glad.h>
+#include "camera.h"
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// camera - give pretty starting point
+Camera camera(glm::vec3(67.0f, 60.5f, 169.9f),
+              glm::vec3(0.0f, 1.0f, 0.0f),
+              -128.1f, -42.4f);
+float deltaTime = 0.0f;
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int useWireframe = 0;
+int displayGrayscale = 0;
+
+float lastX = 800 / 2.0f;
+float lastY = 600 / 2.0f;
+bool firstMouse = true;
+
+// timing
+float lastFrame = 0.0f;
+
+class DisplayManager
+{
+
+public:
+    static void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+    {
+        // make sure the viewport matches the new window dimensions; note that width and
+        // height will be significantly larger than specified on retina displays.
+        int framebuffer_width, framebuffer_height;
+        glfwGetFramebufferSize(window, &framebuffer_width, &framebuffer_height);
+        glViewport(0, 0, framebuffer_width, framebuffer_height);
+    }
+
+    void processInput(GLFWwindow *window)
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+            camera.ProcessKeyboardMovement(FORWARD, deltaTime);
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+            camera.ProcessKeyboardMovement(BACKWARD, deltaTime);
+        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+            camera.ProcessKeyboardMovement(LEFT, deltaTime);
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+            camera.ProcessKeyboardMovement(RIGHT, deltaTime);
+
+        // camera rotation
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+            camera.ProcessKeyboardRotation(1, 0.0, 1);
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+            camera.ProcessKeyboardRotation(-1, 0.0, 1);
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+            camera.ProcessKeyboardRotation(0.0, 1.0, 1);
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+            camera.ProcessKeyboardRotation(0.0, -1.0, 1);
+    };
+
+    GLFWwindow *createWindow()
+    {
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL: Terrain CPU", NULL, NULL);
+
+        if (window == NULL)
+        {
+            glfwTerminate();
+            throw std::runtime_error("Failed to create GLFW window\n");
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        // tell GLFW to capture our mouse
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+        return window;
+    }
+};
+#endif
