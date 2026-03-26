911862811d0f1961248970b46dca0bf2a147955c
moiravs <moira.vanderslagmolen@ulb.be>
2026-03-26 10:44:03 +0100

    [ADD] tree object

--- project/game_engine/camera.h
+++ project/game_engine/camera.h
@@ -20,7 +20,7 @@ enum Camera_Movement
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
 const float MOV_SPEED = 100.0f;
-const float ROT_SPEED = 2.5f;
+const float ROT_SPEED = 1.0f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
 
