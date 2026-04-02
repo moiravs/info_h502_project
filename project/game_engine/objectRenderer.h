66e47a1a58bce67d4214e5d48fc8b0fd3660b0ce
moiravs <moira.vanderslagmolen@ulb.be>
2026-04-02 10:46:06 +0200

    [REFACTOR] tree set height automatically

--- project/game_engine/objectRenderer.h
+++ project/game_engine/objectRenderer.h
@@ -18,91 +18,89 @@
 class ObjectRenderer
 {
 public:
-    std::vector<Vertex> vertices;
-
-    size_t numVertices;
-
     GLuint VBO, VAO;
     bool transparent;
     std::vector<glm::mat4> models;
     Shader shader;
     Texture tex;
+    std::vector<Object> objects;
+    glm::mat4 model = glm::mat4(1.0);
 
-    ObjectRenderer(Shader shader, Object object, std::vector<glm::mat4> models, Texture tex) : shader(shader), vertices(object.getVertices()), models(models), tex(tex)
+    ObjectRenderer(Shader shader, std::vector<Object> objects, Texture tex) : shader(shader), tex(tex), objects(objects)
     {
 
-        // BUG
-
-        // Normally should work because wall is passed in parameter with tex but does not work :((
-        //
-        tex = Texture(PATH_TO_SRC "/../assets/textures/wall.jpg");
-        this->models = models;
-        numVertices = vertices.size();
-
-        auto *data = new float[8 * numVertices];
-        for (int i = 0; i < numVertices; i++)
+        for (auto object : objects)
         {
-            const Vertex v = vertices.at(i);
-            data[i * 8] = v.Position.x;
-            data[i * 8 + 1] = v.Position.y;
-            data[i * 8 + 2] = v.Position.z;
 
-            data[i * 8 + 3] = v.Texture.x;
-            data[i * 8 + 4] = v.Texture.y;
+            tex = Texture(PATH_TO_SRC "/../assets/textures/wall.jpg");
+            int numVertices = object.numVertices;
+            this->models = models;
 
-            data[i * 8 + 5] = v.Normal.x;
-            data[i * 8 + 6] = v.Normal.y;
-            data[i * 8 + 7] = v.Normal.z;
+            auto *data = new float[8 * numVertices];
+            for (int i = 0; i < numVertices; i++)
+            {
+                const Vertex v = object.vertices.at(i);
+                data[i * 8] = v.Position.x;
+                data[i * 8 + 1] = v.Position.y;
+                data[i * 8 + 2] = v.Position.z;
+
+                data[i * 8 + 3] = v.Texture.x;
+                data[i * 8 + 4] = v.Texture.y;
+
+                data[i * 8 + 5] = v.Normal.x;
+                data[i * 8 + 6] = v.Normal.y;
+                data[i * 8 + 7] = v.Normal.z;
+            }
+
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+
+            // define VBO and VAO as active buffer and active vertex array
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, static_cast<long>(sizeof(Vertex) * numVertices), data, GL_STATIC_DRAW);
+
+            auto att_pos = glGetAttribLocation(shader.ID, "position");
+            glEnableVertexAttribArray(att_pos);
+            glVertexAttribPointer(att_pos, 3, GL_FLOAT, false, 8 * sizeof(float), nullptr);
+
+            auto att_col = glGetAttribLocation(shader.ID, "normal");
+            glEnableVertexAttribArray(att_col);
+            glVertexAttribPointer(att_col, 3, GL_FLOAT, false, 8 * sizeof(float),
+                                  reinterpret_cast<void *>(5 * sizeof(float)));
+
+            auto att_tex = glGetAttribLocation(shader.ID, "tex_coord");
+            glEnableVertexAttribArray(att_tex);
+            glVertexAttribPointer(att_tex, 2, GL_FLOAT, false, 8 * sizeof(float),
+                                  reinterpret_cast<void *>(3 * sizeof(float)));
+
+            // desactive the buffer
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+            delete[] data;
         }
-
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-
-        // define VBO and VAO as active buffer and active vertex array
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, static_cast<long>(sizeof(Vertex) * numVertices), data, GL_STATIC_DRAW);
-
-        auto att_pos = glGetAttribLocation(shader.ID, "position");
-        glEnableVertexAttribArray(att_pos);
-        glVertexAttribPointer(att_pos, 3, GL_FLOAT, false, 8 * sizeof(float), nullptr);
-
-        auto att_col = glGetAttribLocation(shader.ID, "normal");
-        glEnableVertexAttribArray(att_col);
-        glVertexAttribPointer(att_col, 3, GL_FLOAT, false, 8 * sizeof(float),
-                              reinterpret_cast<void *>(5 * sizeof(float)));
-
-        auto att_tex = glGetAttribLocation(shader.ID, "tex_coord");
-        glEnableVertexAttribArray(att_tex);
-        glVertexAttribPointer(att_tex, 2, GL_FLOAT, false, 8 * sizeof(float),
-                              reinterpret_cast<void *>(3 * sizeof(float)));
-
-        // desactive the buffer
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-        delete[] data;
     }
 
     void draw() const
     {
-        if (transparent)
-        {
-            glEnable(GL_BLEND);
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        }
-        glBindVertexArray(this->VAO);
-        glDrawArrays(GL_TRIANGLES, 0, static_cast<int>(numVertices));
     }
 
     void render()
     {
 
         tex.bind();
-        for (auto i : models)
+        for (auto obj : objects)
         {
             this->shader.use();
-            this->shader.updatePos(camera, i);
-            this->draw();
+            this->shader.setMatrix4("model", obj.model);
+            this->shader.updatePos(camera);
+            if (transparent)
+            {
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            }
+            glBindVertexArray(this->VAO);
+            glDrawArrays(GL_TRIANGLES, 0, static_cast<int>(obj.numVertices));
         }
         tex.unbind();
     }
