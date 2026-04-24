f138514c4dd66aeff7eca86c2300b7769277a924
moiravs <moira.vanderslagmolen@ulb.be>
2026-04-24 15:53:09 +0200

    [REFACTOR] to particle generator

--- project/game_engine/particleGenerator.h
+++ project/game_engine/particleGenerator.h
@@ -29,30 +29,38 @@ struct Particle
 // ParticleGenerator acts as a container for rendering a large number of
 // particles by repeatedly spawning and updating particles and killing
 // them after a given amount of time.
-// class ParticleGenerator
-// {
-// public:
-//     // constructor
-//     ParticleGenerator(Shader shader, Texture texture, unsigned int amount);
-//     // update all particles
-//     void Update(float dt, Object &object, unsigned int newParticles, glm::vec3 offset = glm::vec3(0.0f, 0.0f, 0.0f));
-//     // render all particles
-//     void Draw();
+const int MaxParticles = 10000;
 
-// private:
-//     // state
-//     std::vector<Particle> particles;
-//     unsigned int amount;
-//     // render state
-//     Shader shader;
-//     Texture texture;
-//     unsigned int VAO;
-//     // initializes buffer and vertex attributes
-//     void init();
-//     // returns the first Particle index that's currently unused e.g. Life <= 0.0f or 0 if no particle is currently inactive
-//     unsigned int firstUnusedParticle();
-//     // respawns particle
-//     void respawnParticle(Particle &particle, Object &object, glm::vec3 offset = glm::vec3(1.0f, 1.0f, 1.0f));
-// };
+class ParticleGenerator
+{
+public:
+    int findUnusedParticle();
+    void sortParticles();
+    Particle particlesContainer[MaxParticles];
+
+    int lastUsedParticle = 0;
+
+    //     // constructor
+    //     ParticleGenerator(Shader shader, Texture texture, unsigned int amount);
+    //     // update all particles
+    //     void Update(float dt, Object &object, unsigned int newParticles, glm::vec3 offset = glm::vec3(0.0f, 0.0f, 0.0f));
+    //     // render all particles
+    //     void Draw();
+
+    // private:
+    //     // state
+    //     std::vector<Particle> particles;
+    //     unsigned int amount;
+    //     // render state
+    //     Shader shader;
+    //     Texture texture;
+    //     unsigned int VAO;
+    //     // initializes buffer and vertex attributes
+    //     void init();
+    //     // returns the first Particle index that's currently unused e.g. Life <= 0.0f or 0 if no particle is currently inactive
+    //     unsigned int firstUnusedParticle();
+    //     // respawns particle
+    //     void respawnParticle(Particle &particle, Object &object, glm::vec3 offset = glm::vec3(1.0f, 1.0f, 1.0f));
+};
 
 #endif
