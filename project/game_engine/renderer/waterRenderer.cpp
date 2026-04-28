4ffe3c58aac7732c37a982e5780c3cc5cc460e91
moiravs <moira.vanderslagmolen@ulb.be>
2026-04-28 22:24:04 +0200

    [CLEAN]

--- project/game_engine/renderer/waterRenderer.cpp
+++ project/game_engine/renderer/waterRenderer.cpp
@@ -14,48 +14,6 @@ void WaterRenderer::registerEntity(const std::shared_ptr<RenderableEntity> &enti
     this->_shader->setInteger("dudvMap", 2);
 }
 
-void WaterRenderer::setupVAOs()
-{
-    // define VBO and VAO as active buffer and active vertex array
-    const auto &mesh = this->getEntity<Object>()->getMesh();
-
-    if (mesh->m_Entries.empty())
-        return;
-
-    this->createVAOs(1);
-    this->createVBOs(1);
-
-    glBindVertexArray(_VAOs[0]);
-
-    // USE THE BUFFER FROM THE MESH ENTRY, NOT _VBOs[0]
-    glBindBuffer(GL_ARRAY_BUFFER, mesh->m_Entries[0].VB);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->m_Entries[0].IB);
-
-    auto att_pos = glGetAttribLocation(_shader->getID(), "position");
-    if (att_pos >= 0)
-    {
-        glEnableVertexAttribArray(att_pos);
-        glVertexAttribPointer(att_pos, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, position));
-    }
-
-    auto att_nor = glGetAttribLocation(_shader->getID(), "normal");
-    if (att_nor >= 0)
-    {
-        glEnableVertexAttribArray(att_nor);
-        glVertexAttribPointer(att_nor, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, normal));
-    }
-
-    auto att_tex = glGetAttribLocation(_shader->getID(), "tex_coord");
-    if (att_tex >= 0)
-    {
-        glEnableVertexAttribArray(att_tex);
-        glVertexAttribPointer(att_tex, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, texture));
-    }
-    // desactive the buffer
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-}
-
 void WaterRenderer::render()
 {
     const auto o = this->getEntity<Object>();
@@ -63,13 +21,6 @@ void WaterRenderer::render()
         return;
 
     Renderer::render();
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, _fbos->getReflectionTexture());
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, _fbos->getRefractionTexture());
-    glActiveTexture(GL_TEXTURE2);
-    glBindTexture(GL_TEXTURE_2D, dudvMap.getTexture());
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -86,4 +37,10 @@ void WaterRenderer::render()
 
 void WaterRenderer::updateUniforms() const
 {
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, _fbos->getReflectionTexture());
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, _fbos->getRefractionTexture());
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, dudvMap.getTexture());
 }
