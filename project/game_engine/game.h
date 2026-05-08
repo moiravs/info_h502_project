f12bd1d40395daf60860c5639b2873a9b237f1c7
moiravs <moira.vanderslagmolen@ulb.be>
2026-05-09 01:09:20 +0200

    [ADD] text

--- project/game_engine/game.h
+++ project/game_engine/game.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "entity/renderableEntity.h"
+#include "entity/text.h"
 #include "shader.h"
 
 class HeightMap;
@@ -16,6 +17,7 @@ public:
     static void renderScene(float delta, const std::vector<std::shared_ptr<Renderable>> &renderers);
     static void checkTerrainCollision(const std::shared_ptr<Entity> &entity, const std::shared_ptr<HeightMap> &heightMap);
     static void renderShadows(const float delta, const std::vector<std::shared_ptr<Renderable>> &renderers, glm::mat4 lightSpaceMatrix, unsigned int shadowTex, const std::shared_ptr<Shader> shadowShader);
+    static void renderText(Text characters, Shader &shader, std::string text, float x, float y, float scale, glm::vec3 color);
 };
 
 #endif
